4562eff442ed5822d37762c2eb22edc85f0ea276
Anuroop Gowda C <118721095+Anuroopgowda@users.noreply.github.com>
2025-07-29 18:39:20 +0530

    Add files via upload

---
+++ 29-07-2025/Lily's_Homework.py
@@ -0,0 +1,35 @@
+import os
+
+
+def lilysHomework(arr):
+    ascending = sorted(arr)
+    descending = ascending[::-1]
+
+    def count_swaps(target):
+        index_map = {value: idx for idx, value in enumerate(target)}
+        arr_copy = arr[:]
+        visited = [False] * len(arr_copy)
+        swap_count = 0
+
+        for i in range(len(arr_copy)):
+            if visited[i] or arr_copy[i] == target[i]:
+                continue
+            cycle_size = 0
+            j = i
+            while not visited[j]:
+                visited[j] = True
+                j = index_map[arr_copy[j]]
+                cycle_size += 1
+            if cycle_size > 0:
+                swap_count += (cycle_size - 1)
+        return swap_count
+
+    swaps_asc = count_swaps(ascending)
+    swaps_desc = count_swaps(descending)
+
+    return min(swaps_asc, swaps_desc)
+
+
+
+
+   
